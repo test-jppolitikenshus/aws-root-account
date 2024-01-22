2830f04ede8c0515be9ee45f89f1a3ae4171dfb2
Kristoffer Egefelt <kristoffer.egefelt@jppol.dk>
2024-01-22 09:21:46 +0100

    cleanup

--- terraform/github/backend.tf
+++ terraform/github/backend.tf
@@ -1,22 +1,9 @@
 terraform {
   required_version = "<1.6"
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "=5.32.1"
-    }
     github = {
       source  = "integrations/github"
       version = "6.0.0-alpha"
-    }
-  }
-}
-
-provider "aws" {
-  default_tags {
-    tags = {
-      owner     = local.github_org
-      terraform = "true"
     }
   }
 }
