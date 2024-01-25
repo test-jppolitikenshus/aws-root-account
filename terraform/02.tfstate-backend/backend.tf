8a1cdeb350141ab4f32ec08dc0f998e0b995452d
Kristoffer Egefelt <kristoffer.egefelt@jppol.dk>
2024-01-25 21:28:53 +0100

    Update required versions and providers in backend.tf

--- terraform/02.tfstate-backend/backend.tf
+++ terraform/02.tfstate-backend/backend.tf
@@ -1,6 +1,10 @@
 terraform {
-  required_version = ">= 1.0.0"
-
+  required_version = "< 1.6.0"
+  required_providers {
+    local = { version = "2.4.1" }
+    aws   = { version = "5.33.0" }
+    time  = { version = "0.10.0" }
+  }
   backend "s3" {
     region  = "eu-north-1"
     bucket  = "pt-test-terraform-state"
@@ -11,3 +15,7 @@ terraform {
     dynamodb_table = "pt-test-terraform-state-lock"
   }
 }
+
+
+
+
