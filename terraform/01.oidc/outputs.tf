1cbd6f04549a330c3acf685bc314ef256d819579
Kristoffer Egefelt <kristoffer.egefelt@jppol.dk>
2024-01-27 20:21:38 +0100

    Add output variables for github_org and aws_root_account in 01.oidc

---
+++ terraform/01.oidc/outputs.tf
@@ -0,0 +1,11 @@
+output "github_org" {
+  description = "Which github organisation is configured"
+  value       = var.github_org
+}
+
+output "aws_root_account" {
+  description = "Which root account is configured"
+  value       = var.aws_root_account
+}
+
+
