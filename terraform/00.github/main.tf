34ef9b69a4ab1de00278950301c5ce380ffceb91
Kristoffer Egefelt <kristoffer.egefelt@jppol.dk>
2024-02-03 18:13:22 +0100

    Refactor file paths and update output description in main.tf

--- terraform/00.github/main.tf
+++ terraform/00.github/main.tf
@@ -16,6 +16,6 @@ data "external" "asdf" {
 }
 
 output "asdfasfd" {
-  description = "asdfasdfasdasdasdasdffff"
+  description = "asdfasdfasdasdasdasdasdfffff"
   value       = data.external.asdf.program
 }
