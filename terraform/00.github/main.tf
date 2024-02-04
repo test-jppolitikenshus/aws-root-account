163df0abf01aba210d7f26f61ffe5d6d9bf15131
Kristoffer Egefelt <kristoffer.egefelt@jppol.dk>
2024-02-04 12:45:29 +0100

    Update description in output "asdfasfd"

--- terraform/00.github/main.tf
+++ terraform/00.github/main.tf
@@ -16,6 +16,6 @@ data "external" "asdf" {
 }
 
 output "asdfasfd" {
-  description = "asdfasdfasdasdasdasdasdasdasdasdasdasfasasdfddfffffffffasdf"
+  description = "asdfasdfasdasdasdasdasdasdasdasdasdasfasasdfddfffffffffasdasfdf"
   value       = data.external.asdf.program
 }
