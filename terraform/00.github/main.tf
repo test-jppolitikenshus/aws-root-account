afe74bd0eb6f14dc02cdbf04a64938fa75a6c339
Kristoffer Egefelt <kristoffer.egefelt@jppol.dk>
2024-02-04 14:00:09 +0100

    Update output description in main.tf and add a new line in oidc/main.tf

--- terraform/00.github/main.tf
+++ terraform/00.github/main.tf
@@ -16,6 +16,6 @@ data "external" "asdf" {
 }
 
 output "asdfasfd" {
-  description = "asdfasdfasdasdasdasdasdasdasdasdasdasfasasdfddfffffffffasdasfasdfdf"
+  description = "asdfasdfasdasdasdasdasdasdasdasdasdasfasasdfddfffffffffasdasfasdasdffdf"
   value       = data.external.asdf.program
 }
