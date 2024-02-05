35c90a0c58d923843831561af5e03fd81c9cd340
Kristoffer Egefelt <kristoffer.egefelt@jppol.dk>
2024-02-05 20:30:10 +0100

    Refactor environment variables and output descriptions in AWS root account plan and Terraform main.tf files

--- terraform/00.github/main.tf
+++ terraform/00.github/main.tf
@@ -16,6 +16,6 @@ data "external" "asdf" {
 }
 
 output "asdfasfd" {
-  description = "asdfasdfasdasdasdasdasdasdasdasdasdasfasasdfddfffffffffasdasfasdasdasdasdasdfffffdf"
+  description = "asdfasdfasdasdasdasdasdasdasdasdasdasfasasdfddfffffffffasdasfasdasdasdasdasdasdffffffdf"
   value       = data.external.asdf.program
 }
