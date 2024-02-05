7cd887c81a85dbcc6160c5be4e575f969e3c5da9
Kristoffer Egefelt <kristoffer.egefelt@jppol.dk>
2024-02-05 20:41:42 +0100

    Update environment variables in AWS root account plan and main.tf

--- terraform/00.github/main.tf
+++ terraform/00.github/main.tf
@@ -16,6 +16,6 @@ data "external" "asdf" {
 }
 
 output "asdfasfd" {
-  description = "asdfasdfasdasdasdasdasdasdasdasdasdasfasasdfddfffffffffasdasfasdasdasdasdasdasdasdfffffffdf"
+  description = "asdfasdfasdasdasdasdasdasdasdasdasdasfasasdfddfffffffffasdasfasdasdasdasdasdasdasdasdffffffffdf"
   value       = data.external.asdf.program
 }
