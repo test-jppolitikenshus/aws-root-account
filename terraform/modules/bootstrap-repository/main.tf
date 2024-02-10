6d1624c5e9f9647a15dd2cda58ef14206a5b4dcf
Kristoffer Egefelt <kristoffer.egefelt@jppol.dk>
2024-02-10 08:04:24 +0100

    Add github organization and cost center for test environment

--- terraform/modules/bootstrap-repository/main.tf
+++ terraform/modules/bootstrap-repository/main.tf
@@ -71,7 +71,7 @@ resource "github_repository" "default" {
 resource "github_branch_protection" "default" {
   repository_id          = github_repository.default.id
   pattern                = "main"
-  enforce_admins         = true
+  enforce_admins         = false # allows admin to bypass PR
   require_signed_commits = false # true prevents terraform from provisioning
 
   required_status_checks {
