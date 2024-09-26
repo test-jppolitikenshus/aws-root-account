722cb6c59ba3d82f27b8a0cfa9bf23472f228a68
Kristoffer Egefelt <kristoffer.egefelt@jppol.dk>
2024-09-26 22:46:32 +0200

    Add skip for CKV2_AWS_38 in aws_route53_zone resource definition in main.tf

--- terraform/03.template/main.tf
+++ terraform/03.template/main.tf
@@ -4,7 +4,7 @@
 #}
 
 #checkov:skip=CKV2_AWS_39:"very good explanation here"
-#checkov:skip=CKV2_AWS_38:"very good explanation here"
 resource "aws_route53_zone" "example_zone" {
+  #checkov:skip=CKV2_AWS_38:"very good explanation here"
   name = "hest.test"
 }
