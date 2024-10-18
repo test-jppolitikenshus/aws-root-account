8f217d60134d242f5d1b5c200195abe3afa6a509
Kristoffer Egefelt <kristoffer.egefelt@jppol.dk>
2024-10-18 15:32:20 +0200

    Update AWS account alternate contact resources with specific details

--- terraform/03.template/main.tf
+++ terraform/03.template/main.tf
@@ -3,32 +3,32 @@
 #  instance_type = "t1.micro"
 #}
 
-resource "aws_route53_zone" "example_zone" {
-  #checkov:skip=CKV2_AWS_39:"very good explanation here"
-  #checkov:skip=CKV2_AWS_38:"very good explanation here"
-  name = "hest.test"
-}
+#resource "aws_route53_zone" "example_zone" {
+#  #checkov:skip=CKV2_AWS_39:"very good explanation here"
+#  #checkov:skip=CKV2_AWS_38:"very good explanation here"
+#  name = "hest.test"
+#}
 
-resource "aws_account_alternate_contact" "testing123" {
+resource "aws_account_alternate_contact" "alt-operations" {
   alternate_contact_type = "OPERATIONS"
-  name                   = "blabla"
-  title                  = "asdf"
-  phone_number           = "234234"
-  email_address          = "asdf@asdf.df"
+  name                   = ""
+  title                  = ""
+  phone_number           = ""
+  email_address          = ""
 }
 
-resource "aws_account_alternate_contact" "testing1234" {
+resource "aws_account_alternate_contact" "alt-billing" {
   alternate_contact_type = "BILLING"
-  name                   = "blabla"
-  title                  = "asdf"
-  phone_number           = "234234"
-  email_address          = "asfd@asdf.dk"
+  name                   = "Finanace department"
+  title                  = "Group Controller"
+  phone_number           = "+4533472244"
+  email_address          = "koncernokonomi@jppol.dk"
 }
 
-resource "aws_account_alternate_contact" "testing12345" {
+resource "aws_account_alternate_contact" "alt-security" {
   alternate_contact_type = "SECURITY"
-  name                   = "blabla"
-  title                  = "asdf"
-  phone_number           = "234234"
-  email_address          = "adsfasasasdadasdsdsdddddsdffffdf@asdf.dk"
+  name                   = ""
+  title                  = ""
+  phone_number           = ""
+  email_address          = ""
 }
