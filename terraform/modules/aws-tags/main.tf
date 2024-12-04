8ed70e1edcb49795cd9f32cb22f9a23373bd1a5a
kristoffer.egefelt <kristoffer.egefelt@jppol.dk>
2024-12-04 15:25:28 +0100

    Fix variable interpolation in main.tf files

--- terraform/modules/aws-tags/main.tf
+++ terraform/modules/aws-tags/main.tf
@@ -8,7 +8,7 @@ provider "aws" {
       "repository"         = var.tf_repo,
       "repository2"        = repository2value,
       "root_module"        = var.tf_module,
-      "aws_account_number" = ${var.tf_aws_account_number},
+      "aws_account_number" = var.tf_aws_account_number,
       "aws_account_name"   = var.tf_aws_account_name,
       "github_org"         = var.tf_github_org
     }
