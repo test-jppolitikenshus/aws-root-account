4b1b914c624b969e458b1134581d837a2b1e5f30
kristoffer.egefelt <kristoffer.egefelt@jppol.dk>
2024-12-05 16:37:15 +0100

    Add GitHub-related variables for Terraform configuration

--- terraform/00.github/variables.tf
+++ terraform/00.github/variables.tf
@@ -17,3 +17,28 @@ variable "tf_module" {
   description = "gets the root module from TF_VAR_tf_module (for the s3 backend)"
 }
 
+variable "tf_aws_account_number" {
+  type        = number
+  description = "AWS account number"
+}
+
+variable "tf_cost_center" {
+  type        = string
+  description = "Cost center testing 123"
+}
+
+variable "tf_github_org" {
+  type        = string
+  description = "GitHub organization"
+}
+
+variable "tf_repo" {
+  type        = string
+  description = "GitHub repository"
+}
+
+variable "tf_pull_request" {
+  type        = string
+  description = "The name of the team"
+  default     = "Cloud Team"
+}
