26c8552deef39b86e31c6a3c89b1f802d16247b9
kristoffer.egefelt <kristoffer.egefelt@jppol.dk>
2024-12-06 15:04:24 +0100

    Update variables for s3 backend and GitHub actions

--- terraform/02.tfstate-backend/variables.tf
+++ terraform/02.tfstate-backend/variables.tf
@@ -1,25 +1,54 @@
+variable "tf_aws_account_name" {
+  type        = string
+  description = "gets the account name from TF_VAR_tf_aws_account_name (for the s3 backend)"
+}
+
 variable "tf_stage" {
   type        = string
-  description = "gets the stage from TF_VAR_tf_stage"
+  description = "gets the stage from TF_VAR_tf_stage (also for the s3 backend)"
   validation {
     condition     = var.tf_stage == "test" || var.tf_stage == "prod"
     error_message = "stage must be either test or prod"
   }
 }
 
-variable "tf_repo" {
+# used in gh action
+# tflint-ignore: terraform_unused_declarations
+variable "tf_module" {
   type        = string
-  description = "gets the repository name from TF_VAR_tf_repo"
+  description = "gets the root module from TF_VAR_tf_module (for the s3 backend)"
 }
 
-#variable "tf_module" {
-#  type        = string
-#  description = "gets the root module from TF_VAR_tf_module"
-#}
-#
-#variable "tf_cost_center" {
-#  type        = number
-#  description = "gets the cost center code from TF_VAR_tf_cost_center"
-#}
+# used in gh action
+# tflint-ignore: terraform_unused_declarations
+variable "tf_aws_account_number" {
+  type        = number
+  description = "AWS account number"
+}
 
+# used in gh action
+# tflint-ignore: terraform_unused_declarations
+variable "tf_cost_center" {
+  type        = string
+  description = "Cost center"
+}
 
+# used in gh action
+# tflint-ignore: terraform_unused_declarations
+variable "tf_github_org" {
+  type        = string
+  description = "GitHub organization"
+}
+
+variable "tf_repo" {
+  type        = string
+  description = "GitHub repository"
+}
+
+# used in gh action
+# tflint-ignore: terraform_unused_declarations
+variable "tf_pull_request" {
+  type        = string
+  description = "The name of the team"
+  default     = "Cloud Team"
+}
