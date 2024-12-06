23b52a7520cf449c47ca44691d56e7ae12f6018c
kristoffer.egefelt <kristoffer.egefelt@jppol.dk>
2024-12-06 19:47:12 +0100

    Update GitHub provider version to 6.4.0 in modules

--- terraform/modules/repository/versions.tf
+++ terraform/modules/repository/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 1.0"
   required_providers {
     github = {
-      version = "6.2.3"
+      version = "6.4.0"
       source  = "integrations/github"
     }
   }
